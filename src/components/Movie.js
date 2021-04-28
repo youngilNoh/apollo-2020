20d7b74ebe0d3c0eb957fc284617469629ff89ce
yesimnoh <yesimnoh@gmail.com>
2021-04-29 08:56:53 +0900

    get movie data from my graphql with apollo

---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Movie({
+  id
+}) {
+  return (
+    <div>
+      <Link to={`/${id}`}>
+        {id}
+      </Link>
+    </div>
+  );
+};
+
+export default Movie;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,37 @@
-export default () => 'detila';
+import React from 'react'
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+function Detail() {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: {
+      id: Number(id)
+    }
+  });
+
+  if (loading) {
+    return "loading"
+  }
+
+  if (data && data.movie) {
+    return data.movie.title
+  }
+  return (
+    <div>Detail</div>
+  );
+}
+
+export default Detail
