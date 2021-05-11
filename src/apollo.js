5b2234bee42a7357786f296921877663c97da330
yesimnoh <yesimnoh@gmail.com>
2021-05-12 08:37:19 +0900

    Mutation 추가

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,22 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+	uri: 'http://localhost:4000/',
+	resolvers: {
+		Movie: {
+			isLiked: () => false,
+		},
+		Mutation: {
+			likeMovie: (_, { id }, { cache }) => {
+				cache.writeData({
+					id: `Movie:${id}`,
+					data: {
+						isLiked: true,
+					},
+				});
+			},
+		},
+	},
 });
 
 export default client;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,34 +1,44 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from "styled-components";
+import styled from 'styled-components';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/client';
+
+const LIKE_MOVIE = gql`
+	mutation likeMovie($id: Int!) {
+		likeMovie(id: $id) @client
+	}
+`;
 
 const Container = styled.div`
-  height: 380px;
-  width: 100%;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
-  border-radius: 7px;
+	height: 400px;
+	width: 100%;
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+	background-color: transparent;
+	border-radius: 7px;
 `;
 
 const Poster = styled.div`
-  background-image: url(${props => props.bg});
-  height: 100%;
-  width: 100%;
-  background-size: cover;
-  background-position: center center;
+	background-image: url(${(props) => props.bg});
+	height: 100%;
+	width: 100%;
+	background-size: cover;
+	background-position: center center;
+	border-radius: 7px;
 `;
 
-function Movie({
-  id,
-  bg,
-}) {
-  return (
-    <Container>
-      <Link to={`/${id}`}>
-        <Poster bg={bg} />
-      </Link>
-    </Container>
-  );
-};
+function Movie({ id, bg, isLiked }) {
+	const [likeMovie] = useMutation(LIKE_MOVIE, { variables: { id: parseInt(id) } });
+	return (
+		<>
+			<Container>
+				<Link to={`/${id}`}>
+					<Poster bg={bg} />
+				</Link>
+				<button onClick={isLiked ? null : likeMovie}>{isLiked ? 'UnLike' : 'Like'}</button>
+			</Container>
+		</>
+	);
+}
 
 export default Movie;
